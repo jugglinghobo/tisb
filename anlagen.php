594323ed94a26050b75c5242ae62841b1376ca79
Simon Kiener <jugglinghobo@gmail.com>
2015-12-02 23:24:59 +0100

    extracted pages, workin on more simplicity

---
+++ anlagen.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <?php include "pages/head.html" ?>
+  <body>
+    <?php include "pages/navigation.html" ?>
+    <div class="container">
+      <?php include "pages/left_column.html" ?>
+      <?php include "pages/anlagen.html" ?>
+    </div>
+  </body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <?php include "pages/head.html" ?>
+  <body>
+    <?php include "pages/navigation.html" ?>
+    <div class="container">
+      <?php include "pages/left_column.html" ?>
+      <?php include "pages/about.html" ?>
+    </div>
+  </body>
+</html>
