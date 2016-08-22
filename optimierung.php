02648351c13a9bcd4b99b19fdda95ae042a2c41e
Simon Kiener <jugglinghobo@gmail.com>
2016-08-22 19:38:52 +0200

    added all pages linked on about page

---
+++ optimierung.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <?php include "pages/head.html" ?>
+  <body>
+    <?php include "pages/navigation.html" ?>
+    <div class="container">
+      <?php include "pages/column_left.html" ?>
+      <?php include "pages/optimierung.html" ?>
+    </div>
+  </body>
+</html>
