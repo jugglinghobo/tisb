34d84c771935e0336fd4ba0852ac89465bdaa3c2
Simon Kiener <jugglinghobo@gmail.com>
2016-09-10 12:31:58 +0200

    WIP nav

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<!-- 
-<!DOCTYPE html>
-<html lang="de">
-  <?php /*include "pages/head.html" */ ?>
-  <body>
-    <div class="container">
-      <div class="content">
-        <div class="column-left">
-          <?php /*include "pages/column_left.html" */?>
-        </div>
-        <div class="column column-center">
-          <?php /*include "pages/about.html" */?>
-        </div>
-        <div class="column">
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
--->
-
 <!DOCTYPE html>
 <html lang="en">
   <?php include "pages/head.html" ?>
@@ -34,14 +13,6 @@
       </div><!-- /.row -->
 
     </div><!-- /.container -->
-
-    <footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
-
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
