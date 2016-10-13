3fe1b92144f510716fd8c934dc9c839dc987b681
Simon Kiener <jugglinghobo@gmail.com>
2016-10-13 23:38:09 +0200

    WIP restyling

--- about/navigation.html.php
+++
@@ -1,3 +0,0 @@
-<div class="img-container logo-medium">
-  <img src="/img/logos/logo-medium-transparent.png" alt="TISB Logo transparent" class="img-responsive center-block" />
-</div>
--- pages/navigation.html.php
+++ pages/navigation.html.php
@@ -1,23 +1,44 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <p class="navbar-text">
-      Technischer<br>
-      Industrie Service für<br>
-      Biomasseanlagen <small>GmbH</small>
-    </p>
-    <ul class="nav navbar-nav center">
-      <li <?php echoSelectedClassIfRequestMatches("about") ?>>
-        <a href="/about.php">Über Uns</a>
-      </li>
-      <li <?php echoSelectedClassIfRequestMatches("anlagen") ?>>
-        <a href="/anlagen.php">Anlagen</a>
-      </li>
-      <li <?php echoSelectedClassIfRequestMatches("kontakt") ?>>
-        <a href="/kontakt.php">Kontakt</a>
-      </li>
-      <li <?php echoSelectedClassIfRequestMatches("partner") ?>>
-        <a href="/partner.php">Partner</a>
-      </li>
-    </ul>
+    <div class="navbar-header hidden-xs hidden-sm">
+      <div class="header-logo-left img-container">
+        <img src="/img/logos/logo-large-transparent-no-text.png" alt="" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-8 col-md-offset-1">
+        <p class="navbar-text">
+          Technischer<br>
+          Industrie Service für<br>
+          Biomasseanlagen <small>GmbH</small>
+        </p>
+      </div>
+      <div class="col-md-3">
+        <p class="navbar-text contact hidden-xs hidden-sm">
+          <strong>TISB GmbH</strong><br>
+          Sonnrainstrasse 84<br>
+          3510 Konolfingen<br>
+          +41 79 299 82 28<br>
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <ul class="nav navbar-nav center">
+            <li <?php echoSelectedClassIfRequestMatches("about") ?>>
+              <a href="/about.php">Über Uns</a>
+            </li>
+            <li <?php echoSelectedClassIfRequestMatches("anlagen") ?>>
+              <a href="/anlagen.php">Anlagen</a>
+            </li>
+            <li <?php echoSelectedClassIfRequestMatches("kontakt") ?>>
+              <a href="/kontakt.php">Kontakt</a>
+            </li>
+            <li <?php echoSelectedClassIfRequestMatches("partner") ?>>
+              <a href="/partner.php">Partner</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </nav>
